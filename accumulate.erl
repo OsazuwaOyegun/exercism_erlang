bf61e1047dcc8a842631e0333e150863835a5de3
Osazuwa Oyegun <zoyegun@gmail.com>
2020-11-28 10:21:55 +0100

    Exercise description
    Convert a number to a string, the contents of which depend on the number's factors.
    
        If the number has 3 as a factor, output 'Pling'.
        If the number has 5 as a factor, output 'Plang'.
        If the number has 7 as a factor, output 'Plong'.
        If the number does not have 3, 5, or 7 as a factor, just pass the number's digits straight through.
    
    Examples
    
        28's factors are 1, 2, 4, 7, 14, 28.
            In raindrop-speak, this would be a simple "Plong".
        30's factors are 1, 2, 3, 5, 6, 10, 15, 30.
            In raindrop-speak, this would be a "PlingPlang".
        34 has four factors: 1, 2, 17, and 34.
            In raindrop-speak, this would be "34".

--- accumulate.erl
+++
@@ -1,8 +0,0 @@
--module(accumulate).
-
--export([accumulate/2]).
-
-accumulate(Fn, Ls)            -> accumulate(Fn, Ls, []).
-
-accumulate(_Fn, [], Result)   -> lists:reverse(Result);
-accumulate(Fn, [H|T], Result) -> accumulate(Fn, T, [Fn(H) | Result]).
--- rna_transcription.erl
+++
@@ -1,9 +0,0 @@
--module(rna_transcription).
-
--export([to_rna/1]).
--define(GET_RNA(X), maps:get(X, #{$G=>$C, $C=>$G, $T=>$A, $A=>$U})).
-
-to_rna(Strand) -> to_rna(Strand, []).
-
-to_rna([], RNA) -> lists:reverse(RNA);
-to_rna([H|T], RNA) -> to_rna(T, [ ?GET_RNA(H) | RNA]).
